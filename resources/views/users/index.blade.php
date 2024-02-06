25fa626c82bd7302739361bd24dc9fae34e64d16
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-02-06 12:20:28 +0700

    One to One relationship

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+</head>
+
+<body class="antialiased">
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+
+        <div class="mt-16">
+            @foreach ($addresses as $address)
+                <h2>{{ $address->country }}</h2>
+                <p>{{ $address->user?->name }}</p>
+            @endforeach
+        </div>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,23 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/users', function () {
+    // $user = User::factory()->create();
+    // $user->address()->create([
+    //     'country' => 'India 1'
+    // ]);
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    // $users = User::with(['address'])->get();
+    $address = Address::with(['user'])->get();
+
+    return view('users.index', compact('address'));
 });
 
 Route::get('/dashboard', function () {
