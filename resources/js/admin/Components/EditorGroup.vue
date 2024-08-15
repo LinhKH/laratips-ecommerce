8839ba85eaece63411ad814e4b30838cd856f90b
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-08-15 22:56:34 +0700

    change from tinymce to ckeditor5

--- resources/js/admin/Components/EditorGroup.vue
+++ resources/js/admin/Components/EditorGroup.vue
@@ -5,6 +5,40 @@ import Editor from "@tinymce/tinymce-vue";
 import InputLabel from "@/admin/Components/InputLabel.vue";
 import InputError from "@/admin/Components/InputError.vue";
 
+
+import {
+	ClassicEditor,
+	AccessibilityHelp,
+	Autosave,
+	Bold,
+	Code,
+	Essentials,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	Highlight,
+	Italic,
+	Paragraph,
+	RemoveFormat,
+	SelectAll,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline,
+	Undo
+} from 'ckeditor5';
+
+import 'ckeditor5/ckeditor5.css';
+import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
+
 defineProps({
     modelValue: {},
     label: {
@@ -20,17 +54,92 @@ defineProps({
 const config = ref({
     height: 300,
 });
+
+const editor = ClassicEditor
+const editorConfig = {
+    toolbar: {
+			items: [
+				'undo',
+				'redo',
+				'|',
+				'selectAll',
+				'|',
+				'fontSize',
+				'fontFamily',
+				'fontColor',
+				'fontBackgroundColor',
+				'|',
+				'bold',
+				'italic',
+				'underline',
+				'strikethrough',
+				'subscript',
+				'superscript',
+				'code',
+				'removeFormat',
+				'|',
+				'specialCharacters',
+				'highlight',
+				'|',
+				'accessibilityHelp'
+			],
+			shouldNotGroupWhenFull: false
+		},
+		plugins: [
+			AccessibilityHelp,
+			Autosave,
+			Bold,
+			Code,
+			Essentials,
+			FontBackgroundColor,
+			FontColor,
+			FontFamily,
+			FontSize,
+			Highlight,
+			Italic,
+			Paragraph,
+			RemoveFormat,
+			SelectAll,
+			SpecialCharacters,
+			SpecialCharactersArrows,
+			SpecialCharactersCurrency,
+			SpecialCharactersEssentials,
+			SpecialCharactersLatin,
+			SpecialCharactersMathematical,
+			SpecialCharactersText,
+			Strikethrough,
+			Subscript,
+			Superscript,
+			Underline,
+			Undo
+		],
+		fontFamily: {
+			supportAllValues: true
+		},
+		fontSize: {
+			options: [10, 12, 14, 'default', 18, 20, 22],
+			supportAllValues: true
+		},
+}
 </script>
 
 <template>
     <div>
         <InputLabel v-if="label"
                :value="label" />
-        <editor api-key="jpamoi4osp74tob1cw02x5sn5vu8g2hf8b77x8t16kew9tdk"
+        <!-- <editor api-key="jpamoi4osp74tob1cw02x5sn5vu8g2hf8b77x8t16kew9tdk"
                 :model-value="modelValue"
                 @update:model-value="$emit('update:modelValue', $event)"
                 :init="config"
-                v-bind="$attrs" />
+                v-bind="$attrs" /> -->
+            <ckeditor
+                :model-value="modelValue"
+                @update:model-value="$emit('update:modelValue', $event)"
+                v-bind="$attrs"
+                :editor="editor"
+                :config="editorConfig"
+            />
+
         <InputError v-if="errorMessage"
                     class="mt-1"
                     :message="errorMessage" />
--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -6,6 +6,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../../vendor/tightenco/ziggy/dist/vue.m'; //package hỗ trợ viết route name của Laravel trong javascript
 
+import { CkeditorPlugin } from '@ckeditor/ckeditor5-vue';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +17,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(CkeditorPlugin)
             .mount(el);
     },
     progress: {
